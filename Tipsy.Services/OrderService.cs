42d422e968fda2f003c27c725c56dc0af21234ab
Isaac Tillema <Tillema97@gmail.com>
2021-08-28 11:02:13 -0400

    added create and get methods

---
+++ Tipsy.Services/OrderService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tipsy.Data;
+using Tipsy.Models;
+
+namespace Tipsy.Services
+{
+    public class OrderService
+    {
+        private readonly Guid _userId;
+
+        public OrderService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        //Create Method
+        public bool CreateOrder(OrderCreate model)
+        {
+            var entity =
+                new Order()
+                {
+                    UserId = _userId,
+                    IsComplete = model.IsComplete,
+                    OrderNotes = model.OrderNotes,
+                    CreatedUtc = DateTimeOffset.Now
+                };
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Orders.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        //Read Method
+        public IEnumerable<OrderListItem> GetOrders()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .Orders
+                        .Where(e => e.UserId == _userId)
+                        .Select(
+                            e =>
+                                new OrderListItem
+                                {
+                                    OrderId = e.OrderId,
+                                    UserFullName = e.UserFullName,
+                                    CreatedUtc = e.CreatedUtc
+                                }
+                                );
+                return query.ToArray();
+            }
+        }
+    }
+}
