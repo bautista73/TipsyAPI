6cbcd10872c644e553e829b5c4ce11dc7f9285bc
Isaac Tillema <Tillema97@gmail.com>
2021-08-28 11:25:16 -0400

    added requests for orders

---
+++ Tipsy.WebAPI/Controllers/OrderController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Tipsy.Models;
+using Tipsy.Services;
+
+namespace Tipsy.WebAPI.Controllers
+{
+    [Authorize]
+    public class OrderController : ApiController
+    {
+        private OrderService CreateOrderService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var orderService = new OrderService(userId);
+            return orderService;
+        }
+
+        public IHttpActionResult Post(OrderCreate order)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateOrderService();
+
+            if (!service.CreateOrder(order))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        public IHttpActionResult Get()
+        {
+            OrderService orderService = CreateOrderService();
+            var orders = orderService.GetOrders();
+            return Ok(orders);
+        }
+    }
+}
