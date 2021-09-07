349a80dcb945f301a2c40b4628ed71a7a6e9dba6
Brandon Bautista <bbautista73@gmail.com>
2021-09-07 16:07:31 -0500

    Added Payment DB

---
+++ Tipsy.Models/PaymentModels/PaymentCreate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tipsy.Data;
+
+namespace Tipsy.Models.PaymentModels
+{
+    class PaymentCreate
+    {
+        public int OrderId { get; set; }
+        public DateTime PaymentDate { get; set; }
+        public decimal Amount { get; set; }
+        public PayType PaymentType { get; set; }
+    }
+}
