5829d535d1867e5b3e056d277e26dfb9f10b03be
Brandon Bautista <bbautista73@gmail.com>
2021-09-12 18:21:27 -0500

    Fixed errors

--- Tipsy.Services/DrinkService.cs
+++ Tipsy.Services/DrinkService.cs
@@ -3,108 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Tipsy.Data;
-using Tipsy.Models.DrinkModels;
 
 namespace Tipsy.Services
 {
-    public class DrinkService
+    class DrinkService
     {
-        private readonly Guid _userId;
-
-        public DrinkService(Guid userId)
-        {
-            _userId = userId;
-        }
-
-        public bool CreateDrink(DrinksCreate model)
-        {
-            var entity =
-                new Drinks()
-                {
-                    UserId = _userId,
-                    DrinkName = model.DrinkName,
-                    Price = model.Price,
-                    Ingredients = model.Ingredients
-                };
-            using (var ctx = new ApplicationDbContext())
-            {
-                ctx.Drink.Add(entity);
-                return ctx.SaveChanges() == 1;
-            }
-        }
-
-        public IEnumerable<DrinksListItem> GetDrinks()
-        {
-            using (var ctx = new ApplicationDbContext())
-            {
-                var query =
-                    ctx
-                        .Drink
-                        .Where(e => e.UserId == _userId)
-                        .Select(
-                            e =>
-                                new DrinksListItem
-                                {
-                                    DrinkId = e.DrinkId,
-                                    DrinkName = e.DrinkName,
-                                    CreatedUtc = e.CreatedUtc
-                                }
-                        );
-                return query.ToArray();
-            }
-        }
-
-        public DrinksDetail GetDrinkById(int id)
-        {
-            using (var ctx = new ApplicationDbContext())
-            {
-                var entity =
-                    ctx
-                        .Drink
-                        .Single(e => e.DrinkId == id && e.UserId == _userId);
-                return
-                    new DrinksDetail
-                    {
-                        DrinkId = entity.DrinkId,
-                        DrinkName = entity.DrinkName,
-                        Price = entity.Price,
-                        CreatedUtc = entity.CreatedUtc,
-                        ModifiedUtc = entity.ModifiedUtc
-                    };
-            }
-        }
-
-        public bool UpdateDrinks(DrinksEdit model)
-        {
-            using (var ctx = new ApplicationDbContext())
-            {
-                var entity =
-                    ctx
-                        .Drink
-                        .Single(e => e.DrinkId == model.DrinkId && e.UserId == _userId);
-                entity.DrinkName = model.DrinkName;
-                entity.Price = model.Price;
-                entity.Ingredients = model.Ingredients;
-                entity.ModifiedUtc = DateTimeOffset.UtcNow;
-
-                return ctx.SaveChanges() == 1;
-            }
-        }
-
-        public bool DeleteDrink(int drinkId)
-        {
-            using (var ctx = new ApplicationDbContext())
-            {
-                var entity =
-                    ctx
-                        .Drink
-                        .Single(e => e.DrinkId == drinkId && e.UserId == _userId);
-
-                ctx.Drink.Remove(entity);
-
-                return ctx.SaveChanges() == 1;
-            }
-        }
     }
 }
--- Tipsy.WebAPI/Controllers/DrinksController.cs
+++ Tipsy.WebAPI/Controllers/DrinksController.cs
@@ -1,74 +1,11 @@
-﻿using Tipsy.Models;
-using Tipsy.Services;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web.Http;
-using Microsoft.AspNet.Identity;
-using Tipsy.Models.DrinkModels;
+using System.Web;
 
 namespace Tipsy.WebAPI.Controllers
 {
-    [Authorize]
-    public class DrinksController : ApiController
+    public class DrinksController
     {
-        private DrinkService CreateDrinkService()
-        {
-            var userId = Guid.Parse(User.Identity.GetUserId());
-            var drinkService = new DrinkService(userId);
-            return drinkService;
-        } 
-
-        public IHttpActionResult Get()
-        {
-            DrinkService drinkService = CreateDrinkService();
-            var drinks = drinkService.GetDrinks();
-            return Ok(drinks);
-        }
-
-        public IHttpActionResult Post(DrinksCreate drink)
-        {
-            if (!ModelState.IsValid)
-                return BadRequest(ModelState);
-
-            var service = CreateDrinkService();
-
-            if(!service.CreateDrink(drink))
-                return InternalServerError();
-
-            return Ok();
-        }
-
-        public IHttpActionResult Get(int id)
-        {
-            DrinkService drinkService = CreateDrinkService();
-            var drink = drinkService.GetDrinkById(id);
-            return Ok(drink);
-        }
-
-        public IHttpActionResult Put(DrinksEdit drink)
-        {
-            if (!ModelState.IsValid)
-                return BadRequest(ModelState);
-
-            var service = CreateDrinkService();
-
-            if (!service.UpdateDrinks(drink))
-                return InternalServerError();
-
-            return Ok();
-        }
-
-        public IHttpActionResult Delete(int id)
-        {
-            var service = CreateDrinkService();
-
-            if (!service.DeleteDrink(id))
-                return InternalServerError();
-
-            return Ok();
-        }
     }
 }
