72162ef73b9413d408c0a0aaf23f3451f50e94ae
Isaac Tillema <Tillema97@gmail.com>
2021-09-14 18:12:45 -0400

    Manually re-added Users and some Order files

---
+++ Tipsy.Models/OrderEdit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tipsy.Models
+{
+    public class OrderEdit
+    {
+        public int Quantity { get; set; }
+        public List<string> Drinks { get; set; }
+    }
+}
---
+++ Tipsy.Models/UserCreate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tipsy.Models
+{
+    public class UserCreate
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Password { get; set; }
+        public UserStatus Status { get; set; }
+        public int MenuId { get; set; }
+    }
+}
