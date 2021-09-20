be0033e91b24193fc37eee206aa4b2d0e457ea3e
Isaac Tillema <Tillema97@gmail.com>
2021-09-20 18:03:34 -0400

    migration5

---
+++ Tipsy.Data/Migrations/202109202202465_migration5.cs
@@ -0,0 +1,18 @@
+﻿namespace Tipsy.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class migration5 : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Payment", "PaymentDate", c => c.DateTimeOffset(nullable: false, precision: 7));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Payment", "PaymentDate", c => c.DateTime(nullable: false));
+        }
+    }
+}
